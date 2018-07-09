86e9053a5609f4bdb34e2922a6a788b93b161639
rieunity <wcghdpwyl@gmail.com>
2018-07-09 21:08:35 +0800

    Add Spring Security.

--- src/main/java/com/rieunity/RegistrationControllser.java
+++ src/main/java/com/rieunity/RegistrationControllser.java
@@ -1,15 +1,61 @@
 package com.rieunity;
 
+import com.rieunity.persistence.dao.UserRepository;
+import com.rieunity.persistence.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/")
 public class RegistrationControllser {
 
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public  RegistrationControllser(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public String welcomePage() {
         return "welcome";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String showRegistration(Model model) {
+        model.addAttribute(new User());
+        return "registration";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String registrationProcessing(@Valid User user, Errors errors, RedirectAttributes model) {
+        if (errors.hasErrors()) {
+            return "registration";
+        }
+        String encodedPassword = new BCryptPasswordEncoder().encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        userRepository.save(user);
+        model.addAttribute("username", user.getUsername());
+        model.addFlashAttribute("user", user);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLogin() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/submit", method = RequestMethod.GET)
+    public String showSubmit() {
+        return "submit";
     }
 }
--- src/main/java/com/rieunity/persistence/model/User.java
+++ src/main/java/com/rieunity/persistence/model/User.java
@@ -1,12 +1,20 @@
 package com.rieunity.persistence.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Arrays;
+import java.util.Collection;
 
 @Entity
-public class User {
+public class User implements UserDetails {
 
     private static final long serialversionUID = 1L;
 
@@ -18,6 +26,8 @@ public class User {
 
     private String password;
 
+    @NotNull
+    @Size(min = 5, max = 16, message = "{username.size}")
     public String getUsername() {
         return username;
     }
@@ -26,11 +36,38 @@ public class User {
         this.username = username;
     }
 
+    @NotNull
+    @Size(min = 5, max = 60, message = "{password.size}")
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
