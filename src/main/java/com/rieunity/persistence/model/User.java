ec3a0d7eda92a5c8dfeacb0c2b71f3d6b74ee499
rieunity <wcghdpwyl@gmail.com>
2018-07-09 19:35:12 +0800

    Add User model and persistence.

---
+++ src/main/java/com/rieunity/persistence/model/User.java
@@ -0,0 +1,36 @@
+package com.rieunity.persistence.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    private static final long serialversionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
