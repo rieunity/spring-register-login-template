4864275939bb13f16fa5ecfb8d2092e543dfe93b
rieunity <wcghdpwyl@gmail.com>
2018-07-09 15:58:28 +0800

    First commit: web+thymeleaf+jpa+h2

---
+++ src/main/java/com/rieunity/SpringRegisterLoginTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.rieunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRegisterLoginTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRegisterLoginTemplateApplication.class, args);
+    }
+}
